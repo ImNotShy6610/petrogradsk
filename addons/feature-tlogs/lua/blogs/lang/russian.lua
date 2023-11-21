fe5961347839ba052c3d4503cf3b535dc354cb6e
dok6610 <zimov.oleh@yandex.ru>
2023-11-21 17:28:14 +0300

    Сделать перевод логов также изменить цвет панели

---
+++ addons/feature-tlogs/lua/blogs/lang/russian.lua
@@ -0,0 +1,126 @@
+--[[
+
+	bLogs French
+
+	Contributors:
+
+	Leaked By PowerFull
+
+]]
+
+bLogs.Language = {
+
+	permission_failure = "Vous n'avez pas la permission d'exécuter cette action.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Админ Мод",
+
+	-- The button to view all logs stored.
+	all_logs = "Все Логи",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Все Игроки",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Игроки",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Получаем Информацию...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Нету Информации!",
+
+	advanced_search = "Уникальный Поиск",
+	quick_search = "Быстрый Поиск",
+	search = "Поиск",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "Найти SteamID",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Выбрать что-то",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Прыгнуть",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Прыгаем на страницу...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "введите номер страницы (il y a %d pages)",
+
+	cancel = "Отменить",
+
+	error_over_pagenum = "Эта страница журналов не существует",
+	error_not_num = "Это не было числом!",
+
+	error = "Ошибка",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Щелкните правой кнопкой мыши для получения дополнительных опций",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Просмотр журналов игроков",
+
+	-- Copy [something] [to clipboard]
+	copy = "Скопировать %s",
+
+	profile = "Профиль steam",
+
+	left_click_to_copy = "Щелкните левой кнопкой мыши, чтобы скопировать",
+
+	modules = "Модули",
+
+	custom = "Индивидуализированный",
+
+	enter_a_steamid = "Вводим SteamID...",
+
+	error_not_steamid = "Это был не SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "И содержит...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Красочный режим",
+
+	jump_to_date = "Перейти к дате",
+
+	jump_to_date_tooltip = "Щелкните правой кнопкой мыши, чтобы перейти к дате/времени",
+
+	type_date = "Введите дату. Она должна быть в таком формате:ДД/ММ/ГГГГ (%s/%s/%s)",
+
+	error_not_date = "Дата недействительна. Она должна быть в таком формате:ДД/ММ/ГГГГ (%s/%s/%s)",
+
+	type_time = " введите время. Он должен быть в таком формате: чч: мм (%s:%s) (24 часа)\n оставьте пустым, чтобы пропустить время".,
+
+	error_not_time = " это было недопустимое время. Она должна быть в следующем формате: чч: мм (%s:%s) (24 часа)",
+
+	archive = "архив",
+
+	searching = "Поиск",
+
+	search_archive = "Искать в архиве",
+
+	search_archive_warning = "Поиск архива может замедлить работу сервера в локальной базе данных. Использовать только в случае реальной необходимости.",
+
+	logging = "Идентифицировать",
+
+	save = "регистр",
+
+	enabled = "Активный",
+
+	disabled = "Деактивирован",
+
+	disable = "Деактивировать",
+
+	enable = "Активировать",
+
+}
+
+
+
+
+-- by trong's team
